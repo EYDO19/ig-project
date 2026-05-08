aeeb9ad58a054fc83adc6111d90ed4deb6e2fef7
EYDO19 <eyadsayed50@gmail.com>
2026-05-08 21:58:52 +0300

    Update STREAMLIT.py

--- igproject/STREAMLIT.py
+++ igproject/STREAMLIT.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import TruncatedSVD
